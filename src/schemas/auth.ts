50b899e01495f861fa3f6b59b87071220d869ba7
alexwox <alexander.woxstrom@gmail.com>
2025-03-20 14:09:15 +0100

    signup page

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -5,4 +5,17 @@ export const loginSchema = z.object({
   password: z.string().min(8, "Password must be at least 8 characters long"),
 });
 
-export type LoginSchema = z.infer<typeof loginSchema>;
+export const signupSchema = z
+  .object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8, "You need at least 8 characters"),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confimrPassword"],
+  });
+
+export type LoginSchemaType = z.infer<typeof loginSchema>;
+export type SignupSchemaType = z.infer<typeof signupSchema>;
