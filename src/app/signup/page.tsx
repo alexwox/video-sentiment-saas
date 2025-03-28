5759ad08c1b67718cbfe17a87fa2a43ecff63940
alexwox <alexander.woxstrom@gmail.com>
2025-03-28 14:09:48 +0100

    back to this shit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 
 export default function SignupPage() {
-  return;
+  return (
+    <div className="min-h-screen bg-white">
+      <nav className="flex h-16 items-center justify-between border-b border-gray-200 px-10">
+        hi
+      </nav>
+    </div>
+  );
 }
