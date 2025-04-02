ee3572ed619d69c6497ff5ea21e111f521cbb3b6
alexwox <alexander.woxstrom@gmail.com>
2025-04-02 13:40:30 +0200

    upload video component

--- src/components/client/UploadVideo.tsx
+++ src/components/client/UploadVideo.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { FiUpload } from "react-icons/fi";
 
 interface UploadVideoProps {
   apiKey: string;
@@ -15,7 +16,29 @@ function UploadVideo({ apiKey }: UploadVideoProps) {
   return (
     <div className="clex-col flex w-full gap-2">
       <div className="flex w-full cursor-pointer flex-col items-center justify-center gap-2 rounded-xl border border-dashed border-gray-300 p-10">
-        TEst
+        <input
+          type="file"
+          accept="video/mp4,video/mov,video/avi"
+          className="hidden"
+          onChange={(e) => {
+            const file = e.target.files?.[0];
+            if (file) handleUpload(file);
+          }}
+          id="video-upload"
+        ></input>
+        <label
+          htmlFor="video-upload"
+          className="flex cursor-pointer flex-col items-center"
+        >
+          <FiUpload className="min-h-8 min-w-8 text-gray-400" />
+          <h3 className="">
+            {status === "uploading"
+              ? "Uploading..."
+              : status === "analyzing"
+                ? "Analyzing..."
+                : "Upload a video"}
+          </h3>
+        </label>
       </div>
     </div>
   );
