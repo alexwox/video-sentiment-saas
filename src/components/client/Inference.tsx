bdbd2cb454c76bb8ef901a41152c514dcc528a4f
alexwox <alexander.woxstrom@gmail.com>
2025-04-02 14:10:08 +0200

    Handle the upload

--- src/components/client/Inference.tsx
+++ src/components/client/Inference.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import UploadVideo from "./UploadVideo";
 
 interface InferenceProps {
@@ -8,11 +9,21 @@ interface InferenceProps {
   };
 }
 
+export type Analysis = {
+  utterances: {
+    start_time: number;
+    end_time: number;
+    text: string;
+    emotion: Array<{ label: string; confidence: number }>;
+    sentiments: Array<{ label: string; confidence: number }>;
+  };
+};
 export function Inference({ quota }: InferenceProps) {
+  const [analysis, setAnalysis] = useState<Analysis | null>(null);
   return (
     <div className="flex h-fit w-full flex-col gap-3 md:w-1/2">
       <h2 className="text-ls font-medium text-slate-800">Inference</h2>
-      <UploadVideo apiKey={quota.secretKey} />
+      <UploadVideo onAnalysis={setAnalysis} apiKey={quota.secretKey} />
     </div>
   );
 }
--- src/components/client/UploadVideo.tsx
+++ src/components/client/UploadVideo.tsx
@@ -1,20 +1,87 @@
 "use client";
 import React, { useState } from "react";
 import { FiUpload } from "react-icons/fi";
+import { Analysis } from "./Inference";
 
 interface UploadVideoProps {
   apiKey: string;
+  onAnalysis: (analysis: Analysis) => void;
 }
 
-function UploadVideo({ apiKey }: UploadVideoProps) {
+const EMOTION_EMOJI: Record<string, string> = {
+  anger: "😠",
+  disgust: "🤢",
+  fear: "😨",
+  joy: "😊",
+  neutral: "😐",
+  sadness: "😢",
+  suprise: "😲",
+};
+
+function UploadVideo({ apiKey, onAnalysis }: UploadVideoProps) {
   const [status, setStatus] = useState<"idle" | "uploading" | "analyzing">(
     "idle",
   );
   const [error, setError] = useState<string | null>(null);
 
-  const handleUpload = async (file: File) => {};
+  const handleUpload = async (file: File) => {
+    try {
+      setStatus("uploading");
+      setError(null);
+
+      const fileType = `.${file.name.split(".").pop()}`;
+      const res = await fetch("/api/upload-url", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + apiKey,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ fileType: fileType }),
+      }).then((res) => res.json());
+      if (!res.ok) {
+        const error = await res.json();
+        throw new Error(error?.error || "Failed to get upload URL");
+      }
+      const { url, fileId, key } = res.json();
+      // 2. Upload file to S3
+
+      const uploadRes = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "video/mp4" },
+        body: file.type,
+      });
+      if (!uploadRes.ok) {
+        throw new Error("Failed to upload file");
+      }
+
+      setStatus("analyzing");
+
+      // 3. Analyze video
+      const analysisRes = await fetch("/api/sentiment-inference", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + apiKey,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ key }),
+      }).then((res) => res.json());
+
+      if (!analysisRes.ok) {
+        throw new Error("Failed to analyze video");
+      }
+      const analysis = await analysisRes.json();
+      console.log("Analysis; ", analysis);
+      onAnalysis(analysis);
+      setStatus("idle");
+    } catch (error) {
+      setError(error instanceof Error ? error.message : "Upload failed");
+      console.error("Upload failed", error);
+      throw error;
+    }
+  };
+
   return (
-    <div className="clex-col flex w-full gap-2">
+    <div className="flex w-full flex-col gap-2">
       <div className="flex w-full cursor-pointer flex-col items-center justify-center gap-2 rounded-xl border border-dashed border-gray-300 p-10">
         <input
           type="file"
@@ -31,15 +98,19 @@ function UploadVideo({ apiKey }: UploadVideoProps) {
           className="flex cursor-pointer flex-col items-center"
         >
           <FiUpload className="min-h-8 min-w-8 text-gray-400" />
-          <h3 className="">
+          <h3 className="text-md mt-2 font-medium text-slate-800">
             {status === "uploading"
               ? "Uploading..."
               : status === "analyzing"
                 ? "Analyzing..."
                 : "Upload a video"}
           </h3>
+          <p className="text-center text-xs text-gray-500">
+            Get started with sentiment detection by uploading a video
+          </p>
         </label>
       </div>
+      {error && <div className="text-sm text-red-500">{error}</div>}
     </div>
   );
 }
